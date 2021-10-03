a194d6d5c717570f5102dcd03214b22833cddcd3
Cell <1024@lruihao.cn>
2021-10-03 13:50:56 +0800

    Feat: 统一使用 shell 脚本

---
+++ _localtools/auto_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#autour: Lruihao
+cd ..
+git submodule update --remote 
+git add public
+git commit -m "Feat: Update public module commit id"
+git add .
+read -p "Please enter commit message: " commitMsg
+if [ -z $commitMsg ]
+then
+  commitMsg="『菠菜眾長』源码自动更新：$(date +'%F %a %T')"
+fi
+git commit -m "$commitMsg"
+git push
