7331ab32fdb8dc3ec7e596a99148330c02e5dd30
Cell <1024@lruihao.cn>
2021-11-18 14:18:15 +0800

    Feat: 更新自动提交脚本

--- _localadmin/auto_push.sh
+++ _localadmin/auto_push.sh
@@ -7,4 +7,10 @@ if [ -z $commitMsg ];then
   commitMsg="Docs: 『菠菜眾長』内容更新 $(date +'%F %a %T')"
 fi
 git commit -m "$commitMsg"
+read -p "Async submodule public commit id? [y/n]..." async
+if [ $async = 'y' ];then
+  git submodule update --remote 
+  git add public
+  git commit -m "Feat: Update public module commit id"
+fi
 git push
