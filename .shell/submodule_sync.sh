7f6e1de1e28f65d9c9d9159f3a856fe6efe6b9dc
Cell <1024@lruihao.cn>
2022-08-29 21:20:50 +0800

    Chore: 修改 commit message 规则

--- .shell/submodule_sync.sh
+++ .shell/submodule_sync.sh
@@ -2,6 +2,6 @@ echo "Checking for all submodule updates ..."
 cd ..
 git submodule update --remote --merge
 git add themes/FixIt
-git commit -m ":arrow_up: Chore: update FixIt theme version"
+git commit -m ":arrow_up: Chore(theme): update FixIt version"
 git add content/private
 git commit -m ":memo: Docs: update private content"
